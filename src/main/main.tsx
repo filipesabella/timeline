4c3d429b9611661f096f2d021e35183187927ed9
Filipe Sabella <me@filipesabella.com>
2021-09-08 12:51:18 -0300

    Bootstrap

---
+++ src/main/main.tsx
@@ -0,0 +1,13 @@
+import '@fontsource/barlow';
+import 'modern-css-reset';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import '../style/main.css';
+import { App } from './components/App';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
