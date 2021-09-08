bc07c07edde7b18fb81725b2b515b26865c94d20
Filipe Sabella <me@filipesabella.com>
2021-09-08 13:22:57 -0300

    Base layout

--- src/main/components/App.tsx
+++ src/main/components/App.tsx
@@ -1,5 +1,15 @@
 import * as React from 'react';
 
 export const App = () => {
-  return <div>hello</div>;
+  return <div id="app">
+    <div className="buttons">
+      <button>water</button>
+      <button>exercised</button>
+      <button>cooked</button>
+      <button>water</button>
+      <button>exercised</button>
+      <button>cooked</button>
+    </div>
+    <button className="add">Add</button>
+  </div>;
 };
--- src/main/main.tsx
+++ src/main/main.tsx
@@ -2,7 +2,7 @@ import '@fontsource/barlow';
 import 'modern-css-reset';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import '../style/main.css';
+import '../style/main.less';
 import { App } from './components/App';
 
 ReactDOM.render(
