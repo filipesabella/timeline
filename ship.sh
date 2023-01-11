a932a0cb7e1539e0d88d21e7eee2c9f4f89071cc
Filipe Sabella <me@filipesabella.com>
2023-01-11 10:39:49 -0300

    Update ship script

--- ship.sh
+++ ship.sh
@@ -2,7 +2,9 @@ set -e
 
 git checkout build
 git merge master -m "Merge master"
+rm -rf docs/
 yarn build
 git add -A docs
 git commit -m "Release"
-echo "Now git push origin build"
+git push origin build
+git checkout master
